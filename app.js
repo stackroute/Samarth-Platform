deb2c1c6bd71edef283665f8d15fa5c9b6ef8079
Basavaraj K N <basavarajk@Basavarajs-MacBook-Pro.local>
2016-09-23 20:05:07 +0530

    Added npm script to run and added/modified packages and app to run using npm script

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-
 var authRoutes = require('./auth/auth/authRoutes');
 
 var projectRoutes = require('./sectionproject/projectrouter');
@@ -21,54 +20,53 @@ var fieldQCache = require('./questionbox/fieldQCache');
 
 var app = express();
 
+
+app.onAppStart = function(addr) {
+  console.log("Samarth Platform web services is now Running on port:", addr.port);
+
+  mongoose.set('debug', true);
+  /*mongoose.set('debug', function(coll, method, query, doc[, options]) {
+      //do your thing
+  });
+  */
+
+  mongoose.connect('mongodb://localhost:27017/samarthplatformdb');
+
+  //Call any cache loading here if required
+  fieldQCache.loadCache();
+
+}
+
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({
-    extended: false
+  extended: false
 }));
 
-var server = http.createServer(app);
-server.listen(8081);
-
-console.log("Server started...");
-
-mongoose.set('debug', true);
-/*mongoose.set('debug', function(coll, method, query, doc[, options]) {
-    //do your thing
-});
-*/
-mongoose.connect('mongodb://localhost:27017/samarthplatformdb');
-
-//Call any cache loading here if required
-fieldQCache.loadCache();
 
 process.on('SIGINT', function() {
-    console.log("Going to unload all data from field questions cache...!");
-    fieldQCache.clearCache(function() {
-        console.log("Done unloading Field Question Cache ");
-        process.exit(0);
-    });
+  console.log("Going to unload all data from field questions cache...!");
+  fieldQCache.clearCache(function() {
+    console.log("Done unloading Field Question Cache ");
+    process.exit(0);
+  });
 });
 
 
 app.use('*', function(req, res, next) {
-    res.set('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Content-Type, Accept");
-    next();
+  res.set('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header("Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Authorization, Content-Type, Accept");
+  next();
 })
 
 /*app.use('/candidate', authRoutes,userRoutes,projectRoutes,educationRouter,
                    skillRoutes,profilerouter,workRouter,quesRouter,);
 */
 
-app.use('/redistest', function(req, res) {
-    fieldQCache.getFieldQuestion(req.query.sc, req.query.fn, req.query.ln,
-        function(err, data) {
-            return res.json(data);
-        });
-});
 app.use('/candidates', qboxRouter);
 app.use('/candidate', candidateRoutes);
 app.use('/fieldquestions', fieldQRouter);
