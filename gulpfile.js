962a7bc73295e82b15633d3c5a5371bee4103eac
Basavaraj K N <basavarajk@Basavarajs-MacBook-Pro.local>
2016-11-04 21:04:11 +0530

    Added Dev dependencies for linting

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,32 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const htmlhint = require('gulp-htmlhint');
 
-gulp.task('lint', ['eslint','htmlhint']);
+gulp.task('lint', ['eslint', 'htmlhint']);
 
 gulp.task('eslint', function() {
-  return gulp.src(['auth/**/*','candidate/**/*','profiles/**/*','questionbox/**/*','sectioneducation/**/*','sectionpersonalinfo/**/*','sectionproject/**/*','sectionskill/**/*','sectionworkexperiance/**/*','skillcard/**/*','gulpfile.js','!node_modules/**/*'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
+    return gulp.src(['auth/**/*',
+            'candidate/**/*',
+            'profiles/**/*',
+            'questionbox/**/*',
+            'sectioneducation/**/*',
+            'sectionpersonalinfo/**/*',
+            'sectionproject/**/*',
+            'sectionskill/**/*',
+            'sectionworkexperiance/**/*',
+            'skillcard/**/*',
+            'app.js',
+            'gulpfile.js',
+            '!node_modules/**/*'
+        ])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
 });
 
 gulp.task('htmlhint', function() {
-  return gulp.src(['**/*.html','!node_modules/**/*'])
-  .pipe(htmlhint({htmlhintrc: ".htmlhintrc"}))
-  .pipe(htmlhint.failReporter());
+    return gulp.src(['**/*.html', '!node_modules/**/*'])
+        .pipe(htmlhint({
+            htmlhintrc: '.htmlhintrc'
+        }))
+        .pipe(htmlhint.failReporter());
 });
