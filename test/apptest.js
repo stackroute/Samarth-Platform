a437575923710045b5270229d19e1fbe0b89ffb7
MonikaThakur28 <monika.thakur3@wipro.com>
2017-01-14 06:03:54 +0000

    test cases for skill deletion

--- test/apptest.js
+++ test/apptest.js
@@ -5,7 +5,7 @@ var expect = require('chai').expect;
 //Supertest is a library to test apps, which have API endpoints or Request/Response based
 //Supertest wraps "Superagent", HTTP request/response library for server side
 var request = require("supertest");
-
+var server = request.agent("http://localhost:8082");
 //Initilise supertest to tes the module, which you want to test 
 request = request(app);
 
@@ -19,7 +19,7 @@ describe("Describe testing whether the instance is  singleton or not",function (
 		console.log('------>',instance2);
 		expect(instance1).to.equal(instance2);
 		done()
-    });
+	});
 
 })
 
@@ -27,13 +27,33 @@ describe("Describe testing whether the instance is  singleton or not",function (
 //describe -> testscenario
 describe("Make GET request to URL '/' and get a 200 res within 10ms", function() {
 
-    //it -> testcase
-    it('Simple GET Request to root url', function(done) {
-        request.get('/').expect(404, done);
-    });
+	//it -> testcase
+	it('Simple GET Request to root url', function(done) {
+		request.get('/').expect(404, done);
+	});
 
-    it('Testing for not defined route', function(done) {
-        request.get('/_undefined_route').expect(404, done);
-    });
+	it('Testing for not defined route', function(done) {
+		request.get('/_undefined_route').expect(404, done);
+	});
 
-}); //end of describe
+}) //end of describe
+
+describe('API test for skill Deletion',function () {
+  
+it('get a JSON Success(201) response',function(done){
+	   server
+		   .delete('/skill/:candidateid/:skillname')
+		   .expect('Content-Type', 'application/json; charset=utf-8')
+		   .expect(201,done);
+	 });
+})
+
+describe('API test for qualification Deletion',function () {
+  
+it('get a JSON Success(201) response',function(done){
+	   server
+		   .delete('/education/:candidateid/:title')
+		   .expect('Content-Type', 'application/json; charset=utf-8')
+		   .expect(201,done);
+	 });
+});
